9f18b81ff18e844bf3c9a745e3219d189d891084
Luke Shumaker <lukeshu@datawire.io>
2021-10-29 04:55:55 -0600

    Get 1.4 test/service to pass

--- cmd/amb-sidecar/filters/handler/middleware/stub.go
+++ cmd/amb-sidecar/filters/handler/middleware/stub.go
@@ -2,19 +2,98 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/pkg/errors"
 
 	crd "github.com/datawire/apro/apis/getambassador.io/v1beta2"
 	"github.com/datawire/apro/lib/filterapi"
 )
 
-func WithRequestID(ctx context.Context, _ string) context.Context {
-	return ctx
+type requestIDContextKey struct{}
+
+func WithRequestID(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, requestIDContextKey{}, requestID)
 }
 
-func NewTemplatedErrorResponse(_ *crd.ErrorResponse, _ context.Context, _ int, _ error, _ map[string]interface{}) *filterapi.HTTPResponse {
+var fallbackTmpl = &crd.ErrorResponse{
+	Headers: []crd.HeaderFieldTemplate{
+		{Name: "Content-Type", Value: "application/json"},
+	},
+	RawBodyTemplate: `{{ . | json "" }}`,
+}
+
+type errorData map[string]interface{}
+
+func (ed errorData) MarshalJSON() ([]byte, error) {
+	copy := make(map[string]interface{})
+	for k, v := range ed {
+		switch k {
+		case "request_id":
+			httpStatus, httpStatusOK := ed["status_code"].(int)
+			if httpStatusOK && httpStatus/100 == 5 {
+				copy[k] = v
+			}
+		default:
+			copy[k] = v
+		}
+	}
+	return json.Marshal(copy)
+}
+
+func NewTemplatedErrorResponse(
+	tmpl *crd.ErrorResponse,
+	ctx context.Context,
+	httpStatus int,
+	err error,
+	extra map[string]interface{},
+) *filterapi.HTTPResponse {
+	bodyData := errorData{
+		"status_code": httpStatus,
+		"message": err.Error(),
+		"request_id": ctx.Value(requestIDContextKey{}).(string),
+	}
+	for k, v := range extra {
+		if _, set := bodyData[k]; !set {
+			bodyData[k] = v
+		}
+	}
+	header := make(http.Header)
+	for _, field := range tmpl.Headers {
+		value, err := field.Execute(bodyData)
+		if err != nil {
+			return NewTemplatedErrorResponse(fallbackTmpl, ctx, 500,
+				errors.Wrapf(err, "errorResponse: generating header %q", field.Name), nil)
+		}
+		if value != nil {
+			header.Set(field.Name, *value)
+		}
+	}
+
+	bodyTmpl, err := template.
+		New("bodyTemplate").
+		Funcs(template.FuncMap{
+			"json": func(prefix string, data interface{}) (string, error) {
+				bs, err := json.MarshalIndent(data, prefix, "\t")
+				return string(bs), err
+			},
+		}).
+		Parse(tmpl.RawBodyTemplate)
+	if err != nil {
+		panic(fmt.Errorf("the stubs do not support this: error parsing bodyTemplate: %w", err))
+	}
+	body := new(strings.Builder)
+	if err := bodyTmpl.Execute(body, bodyData); err != nil {
+		return NewTemplatedErrorResponse(fallbackTmpl, ctx, 500,
+			errors.Wrap(err, "errorResponse: generating body"), nil)
+	}
+
 	return &filterapi.HTTPResponse{
-		Header: http.Header{},
-		Body:   "",
+		Header: header,
+		Body:   body.String(),
 	}
 }
