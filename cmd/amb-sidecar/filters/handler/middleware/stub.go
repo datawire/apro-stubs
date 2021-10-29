cfc7e2d6912f6869f823670933bee06323313df6
Luke Shumaker <lukeshu@datawire.io>
2021-10-29 04:03:24 -0600

    Get 1.4 test/service to run without panicking

--- cmd/amb-sidecar/filters/handler/middleware/stub.go
+++ cmd/amb-sidecar/filters/handler/middleware/stub.go
@@ -2,15 +2,19 @@ package middleware
 
 import (
 	"context"
+	"net/http"
 
 	crd "github.com/datawire/apro/apis/getambassador.io/v1beta2"
 	"github.com/datawire/apro/lib/filterapi"
 )
 
-func WithRequestID(_ context.Context, _ string) context.Context {
-	return nil
+func WithRequestID(ctx context.Context, _ string) context.Context {
+	return ctx
 }
 
 func NewTemplatedErrorResponse(_ *crd.ErrorResponse, _ context.Context, _ int, _ error, _ map[string]interface{}) *filterapi.HTTPResponse {
-	return nil
+	return &filterapi.HTTPResponse{
+		Header: http.Header{},
+		Body:   "",
+	}
 }
